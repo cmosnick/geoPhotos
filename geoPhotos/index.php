639faa98ba8e83851b4fe4a87cd8fc4bc5c99a8b
Christina Mosnick <cmosnick07@mwc-225173.dhcp.missouri.edu>
2014-12-08 14:08:31 -0600

    Added bootstrap, begacn home page

---
+++ geoPhotos/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Geo Photos</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="startbootstrap-stylish-portfolio-1.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="startbootstrap-stylish-portfolio-1.0.0/css/stylish-portfolio.css">
+		<style>
+		</style>
+		
+	</head>
+	
+	<body>
+		<div class="container-fluid">
+			<div class= "header">
+				<h1>Welcome to GeoPhotos!</h1>
+			</div>
+			<div class="map">
+
+			</div>
+		</div>
+	</body>
+</html>
